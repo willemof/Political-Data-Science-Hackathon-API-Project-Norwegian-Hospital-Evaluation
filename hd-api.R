7a0987efe3d3e88a8909e6610b03417fafc237b2
willemof <willemofstad@gmail.com>
2022-07-20 00:37:30 +0200

    i go zzz

--- hd-api.R
+++ hd-api.R
@@ -12,39 +12,43 @@ library(stringr)
 url <- "https://api.helsedirektoratet.no/innhold/nki/kvalitetsindikatorer/"
 key <- "1c50d76931ba48f69d177c18eaf3c6a8"
 
+#Meta-dataset
 ds<-GET(url, 
         add_headers("Ocp-Apim-Subscription-Key" = key),
         add_headers("Cache-Control"= "no-cache")) %>% 
   content(as = "text") %>% # extracting the data
   jsonlite::fromJSON(flatten = TRUE) # parsing to dataframe
 
-test <- do.call(rbind, ds$attachments)
-test <- test %>% 
+#Retrieving attachments connected to ds
+api.call <- do.call(rbind, ds$attachments)
+api.call <- api.call %>% 
   filter(fileType == "application/json")
 
-#for Loop that retrieves all quality indicators and saves them to data folder
+#for-Loop that retrieves all quality indicators and saves them to data folder
 #It's a 10 minute download.
-for(i in 1:nrow(test)){
+for(i in 1:nrow(api.call)){
+  
   file.tmp <- paste0("./data/quality_indicators/",
-                     str_extract(test$fileName[i], "^[0-9]+"),
+                     str_extract(api.call$fileName[i], "^[0-9]+"),
                      ".csv")
   
   if(file.exists(file.tmp)){
-    print(noquote(c(file.tmp, noquote("was skipped"))))
-    next}
+    print(noquote(c(file.tmp, noquote("was skipped."))))
+    next
+    }
   
-  tmp <- GET(test$fileUri[i],
+  tmp <- GET(api.call$fileUri[i],
              add_headers("Ocp-Apim-Subscription-Key" = key),
              add_headers("Cache-Control"= "no-cache")) %>% 
     content(as = "text") %>% 
     jsonlite::fromJSON(flatten = TRUE) # parsing to dataframe
   
       write_csv(tmp$AttachmentDataRows, file.tmp)
-      print(noquote(c(file.tmp, noquote("was saved"))))
+      print(noquote(c(file.tmp, noquote("was saved."))))
       Sys.sleep(2+abs(rnorm(1)))
     
   }
-#End of for loop
+#End of for-loop
 
 #makes qi list out of csv files
 qi_files <- list.files("./data/quality_indicators/",
@@ -65,7 +69,7 @@ filter_qi <- clean_qi %>%
 health_region_qi <- filter_qi %>%
 filter(location_name == "Helse Sør-Øst RHF")
 
-#generating a list of unique qi indicators
+#generating a list of unique qi
 qi_name_list <- tibble(datasett = unique(filter_qi$quality_indicator_name))
 write.csv(qi_name_list, file = "./qi_name_list.csv")
 
