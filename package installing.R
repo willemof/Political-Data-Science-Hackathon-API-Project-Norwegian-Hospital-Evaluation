0ee128290746591703e09e3479156e220cfd19e3
willemof <willemofstad@gmail.com>
2022-07-21 12:12:39 +0200

    installing script

---
+++ package installing.R
@@ -0,0 +1,10 @@
+#insall script 
+
+install.packages("httr")
+install.packages("tinytex")
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("kableExtra")
+install.packages("rjstat")
+install.packages("dplyr")
+install.packages("stringr")
